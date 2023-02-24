5e9274a72a87b637e7a1507871679f8c3fbc21dd
xiaokang1009 <54624874+xiaokang1009@users.noreply.github.com>
2023-02-24 14:43:16 +0800

    chore: 配置eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    // 开启setup语法糖语法
+    'vue/setup-compiler-macros': true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    parser: '@typescript-eslint/parser'
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    'vue/comment-directive': 'error',
+    'vue/jsx-uses-vars': 'error',
+    'vue/script-setup-uses-vars': 'error'
+  }
+}
